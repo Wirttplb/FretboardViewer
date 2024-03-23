b5b75867ea115245c9c38fc1a22984f3b4148896
Romain Fournet <romain-fournet@hotmail.fr>
2024-03-22 22:53:21 -0400

    Show applied pedals

--- app.py
+++ app.py
@@ -22,13 +22,15 @@ def generate_scale(key: str):
     start_fret = 0
     end_fret = 12
     # fretboard_data = fretboard.generate_major_scale_as_integers(key, start_fret, end_fret)
-    voicing_nb = 2
-    fretboard_data = fretboard.generate_voicing(e9_chords[0].voicings[voicing_nb])
-    pedals_to_apply = [Pedal.init_from_name(pedal) for pedal in e9_chords[0].voicings[voicing_nb].pedals]
+    chord_nb = 5
+    voicing_nb = 1
+    fretboard_data = fretboard.generate_voicing(e9_chords[chord_nb].voicings[voicing_nb])
+    pedals_to_apply = [Pedal.init_from_name(pedal) for pedal in e9_chords[chord_nb].voicings[voicing_nb].pedals]
+    pedals_as_str = e9_chords[chord_nb].voicings[voicing_nb].pedals
 
     fretboard_data = fretboard.convert_fretboard_scale_to_intervals(key, fretboard_data, pedals_to_apply)
 
-    return fretboard_data
+    return fretboard_data, pedals_as_str
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -41,10 +43,16 @@ def display_fretboard():
         if not current_key:
             current_key = "E"
 
-    fretboard_data = generate_scale(current_key)
+    fretboard_data, pedals_as_str = generate_scale(current_key)
 
     return render_template(
-        "fretboard.html", tuning=fretboard.get_tuning_as_str(), fretboard_data=fretboard_data, nb_strings=len(fretboard_data), keys=keys, current_key=current_key
+        "fretboard.html",
+        tuning=fretboard.get_tuning_as_str(),
+        fretboard_data=fretboard_data,
+        nb_strings=len(fretboard_data),
+        keys=keys,
+        current_key=current_key,
+        pedals_to_apply=pedals_as_str,
     )
 
 
